cf5f1d23fb567862548a25291cffcdb59204980b
Kim yoona <20162921@gm.hannam.ac.kr>
2021-11-30 21:00:28 +0900

    장고 view 탬플릿 구현

--- myhomework12/myhomework12/urls.py
+++ myhomework12/myhomework12/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from yoona.views import index,post_list, post_detail
 
+# 리스트
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('yoona/', post_list),
+    path('yoona/<int:pk>/',post_detail),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+       document_root=settings.MEDIA_ROOT) #두번째는 키워드 지정
+# static은 url리스트를 반환
