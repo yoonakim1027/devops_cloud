c928c8468e4c03e756bb19e4ef48c551cccf3748
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 10:46:21 +0900

    media 변경

--- mydjango13/mydjango13/urls.py
+++ mydjango13/mydjango13/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mydjango13 import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URLS,
+                      document_root settings.MEDIA_ROOT)
