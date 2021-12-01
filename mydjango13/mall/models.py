c966c77c9b4b78c57fac58bdedadda20877301ff
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 11:04:38 +0900

    mall/Shop 모델 및 마이그레이션 생성

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# 한 가게의 정보를 담는 모델이 생성됨
+# 프라이머리 키는 아직 생성 안되어 있음
+
+class Shop(models.Model):
+    # id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True) #설명안할수도 있음
+    photo = models.ImageField(blank=True) # 사진이 없을 수도 있음
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
