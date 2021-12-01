aa8641ad0d7413c4c6cbbee799e0300ec0196810
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 18:46:11 +0900

    mall/Shop에 telephone 필드 추가

--- myhomework13/myhomework13/mall/models.py
+++ myhomework13/myhomework13/mall/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 from django.db.models import ImageField
 
@@ -7,6 +7,8 @@ from django.db.models import ImageField
 class Shop(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+
     photo = ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
