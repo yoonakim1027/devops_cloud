453add17d2d4a5830a67e31fb04cefcd690bda9e
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 14:30:32 +0900

    mall/index 뷰 구현

--- mydjango13/mall/views.py
+++ mydjango13/mall/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
+# 함수의 인자와 return타입에 대해서 미리미리 해두는 것이 더 안정적인 품질
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "mall/index.html")
