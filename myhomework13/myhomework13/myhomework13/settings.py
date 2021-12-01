82feef12f6a925bc94fb0858b4b40fdd7326ea10
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 18:53:52 +0900

    django-debug-toolbar 적용

--- myhomework13/myhomework13/myhomework13/settings.py
+++ myhomework13/myhomework13/myhomework13/settings.py
@@ -31,14 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third apps
+
+    # local apps
+    'mall',
 ]
 
+if DEBUG:
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +58,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE =[
+        'debug_toolbar.middleware.DebugToolbarMiddleware'
+    ] + MIDDLEWARE
+
+
+
 
 ROOT_URLCONF = 'myhomework13.urls'
 
@@ -126,3 +144,5 @@ MEDIR_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1']
--- myhomework13/myhomework13/myhomework13/urls.py
+++ myhomework13/myhomework13/myhomework13/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,11 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root = settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import  debug_toolbar
+    urlpatterns += [
+        path('__debug__/',
+             include(debug_toolbar.urls)),
+    ]
