40a562ed5566ba0da39660e13e9aa9f08a5a1126
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-01 18:30:49 +0900

    생성한 app에 class 모델 설정

--- myhomework13/myhomework13/mall/models.py
+++ myhomework13/myhomework13/mall/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import ImageField
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
