f0facefbf7e3ce5739602efebb20b32fa0084feb
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-02 16:26:56 +0900

    django imagekit 라이브러리를 활용한 썸네일 처리

--- mydjango14/catube/models.py
+++ mydjango14/catube/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 # video _> class는 항상 대문자로 시작
 
+
 class Video(models.Model):
     # 그러면 title을 입력하지 않아도 저장해버림.
-    title = models.CharField(max_length=200) #blank / null을 넣는 순간 옵션 필드로 인지해버림
-    description = models.TextField()# blank를 넣지 않으면 필수 필드가 됨
+    title = models.CharField(max_length=200)  # blank / null을 넣는 순간 옵션 필드로 인지해버림
+    description = models.TextField()  # blank를 넣지 않으면 필수 필드가 됨
 
     # TODO : 업로드되는 파일이 비디오 파일인지 여부를 검사!
     video_file = models.FileField()
     thumbnail_file = models.ImageField()
-
+    thumbnail_file_thumb = ImageSpecField(
+        source="thumbnail_file",
+        processors=[ResizeToFill(800, 400)],
+        format="JPEG",
+        options={"quality": 60},
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # 제목은 최대 200글자
