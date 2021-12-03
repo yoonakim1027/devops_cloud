37896fc400e4913ee846872bb2a0a7f2f4df5be6
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-03 16:25:18 +0900

    telephone에 유효성 검사 추가 후

--- mydjango15/delicious/models.py
+++ mydjango15/delicious/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.core.validators import RegexValidator
+
+
 # regex -> 정규표현식
-
-
-
 
 
 class Shop(models.Model):
@@ -17,9 +16,15 @@ class Shop(models.Model):
     longitude = models.FloatField(verbose_name="경도")  # 경도
 
     # TODO : 전화번호 값인지 여부를 체킹 -> 다음 주쯤~
-    telephone = models.CharField(max_length=15) # 현재 아무 문자열이나 입력가능. 
-
+    telephone = models.CharField(max_length=15,
+                                 validators=[
+                                     RegexValidator(r"^\d{3,4}-?\d{4}-?\d{4}$",
+                                                    message="전화번호를 입력해주세요"),
+                                 ])
+    # validators- > 이 값은 꼭 이거어야만 한다!!
+    # 현재 아무 문자열이나 입력가능.
+    #  RegexValidator(r"\d{4}") 숫자가 4번
+    # RegexValidator(r"\d{3,4}") 3번이나 4번
+    # RegexValidator(r"^\d{3,4}-?\d{4}-?\d{4}$")
     created_at = models.DateTimeField(auto_now_add=True)  # 생성시각
     updated_at = models.DateTimeField(auto_now=True)  # 수정시각
-
-
