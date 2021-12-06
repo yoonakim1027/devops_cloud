20dc3b80be864db24bb5b4bc3f435cadd247dba6
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-06 16:40:43 +0900

    Tag 설정

--- mydjango16/blog/admin.py
+++ mydjango16/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post, Comment, Tag
+
+
+# 등록을 위해서는? 각각의 모델 마다 등록이 필요
+# 등록하는 방법은 admin.site.register
+
+@admin.register(Post)  # 장식자 이용하기
+class PostAdmin(admin.ModelAdmin):  # 상속문법
+    pass
+
+
+# 각각 하나씩 만들어줘야함
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'post', 'message', 'created_at']
+
+
+# 각 모델의 admin -> 상속을 받아야 함
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- mydjango16/blog/models.py
+++ mydjango16/blog/models.py
@@ -4,9 +4,20 @@ from django.db import models
 class Post(models.Model):  # model 상속 / pk : id(int, auto increment 정수)
     title = models.CharField(max_length=200, db_index=True)
     content = models.TextField()
+
+    # 문자열로 쓰면? 현재 app에서 tag를 알아서 찾음
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    # blank=True를 설정해주지 않으면 ?
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # 인스턴스에 대한 문자열 표현을 기대
+    # post.title
+    # print(post) # 내부적으로 post.__str__을 찾아서 호출해서 반환값을 출력
+    # print(post.__str__())
+    def __str__(self) -> str:  # return값은 str(문자열)
+        return self.title
     # pk : id
 
 
@@ -18,7 +29,21 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 # 모델에서는 생성자 정의가 없음
 
 # CASCADE -> 작은 폭포
-# on_delete=models.CASCADE -> 포스팅이 pk있는 곳에서 삭제될때, 같이 삭제 됨
+# on_delete=models.CASCADE -> 포스팅이 pk 있는 곳에서 삭제될때, 같이 삭제 됨
+
+
+class Tag(models.Model):
+    # Tag는 일반적으로 단일 컬럼으로 문자열 정도만 저장
+    # 보통 Tag의 name을 pk로 지정할 수도 있지만~
+
+    name = models.CharField(max_length=100, unique=True)
+    # 데이터베이스에서 현재 Tag 범위 내에서는 절대 같은 name의 tag가 존재 할 수 없음
+    # name - > unique라는 제약사항을 걸게 되는 것
+    # 하나의 태그 - > 다수의 포스팅에 속할 수 있고
+    # 하나의 포스팅 - > 다수의 태그에 속할 수 있음
+
+
