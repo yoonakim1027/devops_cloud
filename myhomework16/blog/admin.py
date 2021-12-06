7c1c8c9bb59e5ecc912609a24199c11c858be114
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-06 22:36:37 +0900

    models을 admin에 장식자를 사용해서 등록

---
+++ myhomework16/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from blog.models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'post', 'message', 'created_at']
