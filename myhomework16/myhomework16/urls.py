1f6ca081e7c5826f6b7cd9bca0f709e62cf7b39e
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-06 23:01:21 +0900

    post_detail, post_list 수정

--- myhomework16/myhomework16/urls.py
+++ myhomework16/myhomework16/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog.views import post_list, post_detail
 
@@ -13,3 +13,9 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
