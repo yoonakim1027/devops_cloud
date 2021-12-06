9e8273ed66d49d619184c684b4354c7bca42f068
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-06 22:37:39 +0900

    admin 수정

--- myhomework16/blog/admin.py
+++ myhomework16/blog/admin.py
@@ -5,9 +5,12 @@ from blog.models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "content", "created_at", "updated_at"]
+    list_display_links = ["title"]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['pk', 'post', 'message', 'created_at']
+    list_display_links = ['post']
+
