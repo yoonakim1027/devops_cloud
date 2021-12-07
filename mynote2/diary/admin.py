06a1490bf254013dc589ebef92b6f8b9bf6684dc
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-07 21:06:43 +0900

    models를 admin에 등록

--- mynote2/diary/admin.py
+++ mynote2/diary/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Post, Comment, Tag
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
