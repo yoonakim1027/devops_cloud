7de05c883402dbcca50448437cce2c06ee709a7f
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-07 10:35:20 +0900

    MEDIA 설정 추가

--- mydjango17/mydjango17/settings.py
+++ mydjango17/mydjango17/settings.py
@@ -132,6 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+# 현재 프로젝트 셋팅에 맞춰서, 어디에 저장할 것인가?
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mydjango17/mydjango17/urls.py
+++ mydjango17/mydjango17/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,9 +7,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# 확장 -> static() 함수의 반환값이 리스트
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
