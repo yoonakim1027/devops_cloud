c2aff9fdcd8d5e4b9ac6d0c6362e9ab95f071e93
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-07 22:06:22 +0900

    웹페이지 생성 : layout, post_list, post_detail

--- mynote2/diary/views.py
+++ mynote2/diary/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from diary.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()  # 전체 포스팅 목록을 얻어올 준비
+    context_data = {
+        "post_list": qs,
+    }
+    return render(request, "diary/post_list.html", {
+        "post_list": qs,  # qs 넘겨주기
+    })
+
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+
+    post = Post.objects.get(pk=pk)		# 전체 포스팅 목록을 얻어올 준비
+    templates_name = "diary/post_list.html"
+    context_data = {
+        "post": post,
+    }
+
+    # 각자의 포스팅에 속한 comment을 받기
+    comment_list = post.comment_set.all() # 위에서 얻어온 포스팅 목록의 post
+    tag_list = post.tag_set.all()
+
+    # render(request, 모델명/모델명_detail (템플릿경로)
+    # 주문이 들어올 때까지 DB에 접근을 안한다!
+    return render(request, "diary/post_detail.html", {
+        "post": post,  # qs 넘겨주기
+        "comment_list": comment_list,
+        "tag_list": tag_list,
+    })
