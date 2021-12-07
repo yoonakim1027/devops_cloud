a56db22660f6082a46d708c02cb56cd9466bf0b6
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-07 23:08:11 +0900

    웹페이지 생성 : layout, post_detail, post_list

--- myhomework17/diary/views.py
+++ myhomework17/diary/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpRequest, HttpResponse
+
+from diary.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    context_data = {
+        "post_list": qs,
+    }
+    return render(request, 'diary/post_list.html', {
+        "post_list": qs,
+    })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)  # 전체 포스팅 목록을 얻어올 준비
+
+    comment_list = post.comment_set.all()
+    tag_list = post.tag_set.all()
+
+    return render(request, "diary/post_detail.html", {
+        "post": post,  # qs 넘겨주기
+        "comment_list": comment_list,
+        "tag_list": tag_list,
+    })
