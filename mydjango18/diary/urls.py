89501fd63c97213c06c98a2b6e3f290e80c9b1d8
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-08 16:55:49 +0900

    tag_detail 뷰 구현

--- mydjango18/diary/urls.py
+++ mydjango18/diary/urls.py
@@ -9,4 +9,5 @@ app_name = 'diary'  # 얘네들은 이제 url reverse 할 때 쓰일 것!
 urlpatterns = [
     path('', views.post_list, name="post_list"),  # post_list의 뷰는 "post_list"라는 이름으로 사용 하겠다 .
     path('<int:pk>/', views.post_detail, name="post_detail"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]
