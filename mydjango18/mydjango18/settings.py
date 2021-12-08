11da30f48dfd9f616b40a239b97123481b078b13
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-08 14:49:09 +0900

    MEDIA 설정 추가 (settings, urls)

--- mydjango18/mydjango18/settings.py
+++ mydjango18/mydjango18/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-!7ww-=52bxt56ds7biw&soeok_-(ybgx_lz2i6gv5a$!2l4w#g
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -61,9 +59,8 @@ MIDDLEWARE = [
 
 if DEBUG:
     MIDDLEWARE = [
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    ] + MIDDLEWARE
-
+                     'debug_toolbar.middleware.DebugToolbarMiddleware',
+                 ] + MIDDLEWARE
 
 ROOT_URLCONF = 'mydjango18.urls'
 
@@ -85,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mydjango18.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -95,7 +91,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -115,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -129,21 +123,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-INTERNAL_IPS =['127.0.0.1']
-
-
+INTERNAL_IPS = ['127.0.0.1']
 
 if DEBUG:
     LOGGING = {
@@ -163,5 +156,3 @@ if DEBUG:
             },
         },
     }
-
-
