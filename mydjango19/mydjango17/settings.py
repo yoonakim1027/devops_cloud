3b842e01b33cc5a9569d8959a87783afed8d5bd6
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-09 16:40:13 +0900

    django-bootstrap5 설치

--- mydjango19/mydjango17/settings.py
+++ mydjango19/mydjango17/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third apps
-
+    'bootstrap5',
     # local apps
     'diary',
 ]
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
