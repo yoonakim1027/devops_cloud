3f0c7efcf19a4431326e9308cd261d0a139815f0
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-12 20:20:25 +0900

    tag_detail 뷰 구현

--- mydjango20/shop/urls.py
+++ mydjango20/shop/urls.py
@@ -7,4 +7,5 @@ urlpatterns =[
     path("<int:pk>/",views.shop_detail, name="shop_detail"),
     path("new/",  views.shop_new, name="shop_new"),
     path("", views.shop_list, name="shop_list"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]
