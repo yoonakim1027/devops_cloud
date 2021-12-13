8b5788ac5c4a734baf93b95dd37ad127c51862ab
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-14 00:44:41 +0900

    새 포스팅 쓰기 버튼 작성

--- myhomework21_2/shop/views.py
+++ myhomework21_2/shop/views.py
@@ -12,19 +12,16 @@ def shop_list(request: HttpRequest) -> HttpResponse:
     query = request.GET.get("query", "")
     category_qs = Category.objects.all()
 
-    category_id:str = request.GET.get("category_id","")
+    category_id: str = request.GET.get("category_id", "")
     if category_id:
         qs = qs.filter(category__pk=category_id)
 
     if query:
         qs = qs.filter(name__icontains=query)
 
-
-
-
     return render(request, "shop/shop_list.html", {
         "shop_list": qs,
-        "category_list":category_qs,
+        "category_list": category_qs,
     })
 
 
@@ -39,21 +36,25 @@ def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
 def shop_new(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = ShopForm(request.POST, request.FILES)
-        if form.is_vaild():
-            saved_post = form.save()
-            return redirect("shop:shop_detail", saved_post.pk)
-        else:
-            form = ShopForm()
+        if form.is_valid():
+            saved_post = form.save()  # 항상 저장이 먼저임
+            # commit=False가 되면, 할당을 못받기 때문에 안돼 !! 무조건 비어있는 상태로
+
+            messages.success(request, "성공적으로 저장했습니다.")
+            return redirect("shop:shop_list")
+
+    else:
+        form = ShopForm()
 
     return render(request, "shop/shop_form.html", {
         "form": form,
     })
 
 
-def shop_edit(request:HttpRequest, pk:int)->HttpResponse:
+def shop_edit(request: HttpRequest, pk: int) -> HttpResponse:
     shop = get_object_or_404(Shop, pk=pk)
 
-    if request.method =="POST":
+    if request.method == "POST":
         form = ShopForm(request.POST, request.FILES, instance=shop)
     else:
         form = ShopForm(instance=shop)
@@ -61,5 +62,5 @@ def shop_edit(request:HttpRequest, pk:int)->HttpResponse:
     # 유효성 검사
     if form.is_valid():
         saved_shop = form.save()
-        messages.success(request,".성공적으로 수정했습니다.")
-        return redirect("shop:shop_detail",saved_shop.pk)
+        messages.success(request, ".성공적으로 수정했습니다.")
+        return redirect("shop:shop_detail", saved_shop.pk)
