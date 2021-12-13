94359a48806509d65066e121fd3edd90a8e95010
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-14 00:37:27 +0900

    category 구현

--- myhomework21_2/shop/views.py
+++ myhomework21_2/shop/views.py
@@ -10,11 +10,21 @@ from shop.models import Shop, Category, Tag
 def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
     query = request.GET.get("query", "")
+    category_qs = Category.objects.all()
+
+    category_id:str = request.GET.get("category_id","")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
+
     if query:
         qs = qs.filter(name__icontains=query)
 
+
+
+
     return render(request, "shop/shop_list.html", {
         "shop_list": qs,
+        "category_list":category_qs,
     })
 
 
