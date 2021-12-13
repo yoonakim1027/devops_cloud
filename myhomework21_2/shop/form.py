fb64bdcc0c281951cddeadef5abdc97828d47e30
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-14 00:20:26 +0900

    오류 수정

--- myhomework21_2/shop/form.py
+++ myhomework21_2/shop/form.py
@@ -1,11 +1,11 @@
-
 from django import forms
 from shop.models import Shop, Category, Tag
+
 
 class ShopForm(forms.ModelForm):
     class Meta:
         model = Shop
-        fiels = [
+        fields = [
             "name",
             "description",
             "tag_set",
