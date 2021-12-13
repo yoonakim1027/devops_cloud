595b8225b69d9254c232762161a8637e12f3bdf2
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-13 20:41:34 +0900

    __str__, Meta.ordering 지정

--- myhomework21_1/shop/models.py
+++ myhomework21_1/shop/models.py
@@ -16,10 +16,36 @@ class Shop(TimestampedModel):
 
     tag_set = models.ManyToManyField('Tag', blank=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]  # 정렬
+        verbose_name = "상점"  # 단수
+        verbose_name_plural = "상점 목록"  # 복수
+
+
+
 
 class Category(TimestampedModel):
     name = models.CharField(max_length=100, unique=True)  # 카테고리 이름은 일반적으로 unique속성을 줌
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "카테고리"  # 단수
+        verbose_name_plural = "카테고리 목록"  # 복수
+
 
 class Tag(TimestampedModel):  # 유일성을 가지기 위해, unique 옵션을 넣음
     name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]  # 좀 더 유의미한 결과를 위해 name오름차순으로 정렬
+        verbose_name = "태그"  # 단수
+        verbose_name_plural = "태그 목록"  # 복수
