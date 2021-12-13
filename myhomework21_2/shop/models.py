06efed38c58d6ac3607898c933a8b64a8b2b321d
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-13 23:34:46 +0900

    __str__, Meta.ordering 지정

--- myhomework21_2/shop/models.py
+++ myhomework21_2/shop/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class TimestamedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
 
@@ -10,13 +12,36 @@ class TimestamedModel(models.Model):
 class Shop(TimestamedModel):
     name = models.CharField(max_length=100, db_index=True)
     description = models.TextField(blank=True)
-    tag_set = models.ManyToManyField('Tag',blank=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['-id']  # 정렬
+        verbose_name = "상점"
+        verbose_name_plural = "상점 목록"
 
 
 class Category(TimestamedModel):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['-id']  # 정렬
+        verbose_name = "카테고리"
+        verbose_name_plural = "카테고리 목록"
+
 
 class Tag(TimestamedModel):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['name']  # 오름정렬
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"
