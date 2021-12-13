02017dee2eee3e3ace891c77ed7add0a8cd862d2
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-14 00:07:23 +0900

    edit, new 뷰 구현

--- myhomework21_2/shop/views.py
+++ myhomework21_2/shop/views.py
@@ -1,20 +1,51 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpRequest, HttpResponse
+
+from shop.form import ShopForm
 from shop.models import Shop, Category, Tag
 
 
-def shop_list(request:HttpRequest)-> HttpResponse:
+def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
-    return render(request, "shop/shop_list.html",{
-        "shop_list":qs,
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
     })
 
 
-def shop_detail(request:HttpRequest, pk:int) -> HttpResponse:
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
     shop = get_object_or_404(Shop, pk=pk)
-    return render(request, "shop/shop_detail.html",{
+    return render(request, "shop/shop_detail.html", {
         "shop": shop,
 
     })
 
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_vaild():
+            saved_post = form.save()
+            return redirect("shop:shop_detail", saved_post.pk)
+        else:
+            form = ShopForm()
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
+
+
+def shop_edit(request:HttpRequest, pk:int)->HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+
+    if request.method =="POST":
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+    else:
+        form = ShopForm(instance=shop)
+
+    # 유효성 검사
+    if form.is_valid():
+        saved_shop = form.save()
+        messages.success(request,".성공적으로 수정했습니다.")
+        return redirect("shop:shop_detail",saved_shop.pk)
