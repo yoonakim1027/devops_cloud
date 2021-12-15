6b869ca3999a0207b9f9d872350caade8a7bed2a
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 15:03:18 +0900

    Post.get_absolute_url 구현을 통해서, CreateView/UpdateView의 디폴트 success_url 활용

--- mydjango23_2/blog/views/cbv.py
+++ mydjango23_2/blog/views/cbv.py
@@ -65,9 +65,9 @@ class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
 
-    def get_success_url(self):
-        post_pk = self.object.pk
-        return reverse("blog:post_detail", args=[post_pk])
+    # def get_success_url(self):
+    #     post_pk = self.object.pk
+    #     return reverse("blog:post_detail", args=[post_pk])
 
 
 post_edit = PostUpdateView.as_view(
