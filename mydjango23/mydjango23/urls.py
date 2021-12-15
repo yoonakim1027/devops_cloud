2cf6986697c4fc4f28b10a3d7b2e5a5e76ab1940
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 10:33:53 +0900

    shop 앱 생성, settings에 추가

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -7,13 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-
-
-
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
-
-
 
 if settings.DEBUG:
     import debug_toolbar
