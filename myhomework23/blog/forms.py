1d9aea9fa3a6021c6d18caa5a7f3ab96aff107ba
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 23:05:17 +0900

    Tag 저장 로직을 PostForm 클래스에 구현

--- myhomework23/blog/forms.py
+++ myhomework23/blog/forms.py
@@ -1,8 +1,27 @@
+import self as self
 from django import forms
 from blog.models import Post, Comment, Category, Tag
 
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField()
+
+    def save(self):
+        saved_post = super().save()
+
+        tag_list = []
+        tags = self.cleaned_data.get("tags","")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag,__ = Tag.objects.get_or_create(name=tag_name)
+
+            tag_list.append(tag)
+
+        saved_post.tag_set.clear()
+        saved_post.tag_set.add(*tag_list)
+        return saved_post
+
+
     class Meta:
         model = Post
         fields = [
