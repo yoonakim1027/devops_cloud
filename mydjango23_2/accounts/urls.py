4c1a58f420e9e694b02e2e222899bf003a093416
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 16:50:29 +0900

    accounts 앱 signup 회원가입 구현

--- mydjango23_2/accounts/urls.py
+++ mydjango23_2/accounts/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
 
-
+from accounts import views
 
 app_name = "accounts"
 
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('profile/', views.profile, name="profile"),
+    path('logout/', views.logout, name="logout"),
+
+
+]
