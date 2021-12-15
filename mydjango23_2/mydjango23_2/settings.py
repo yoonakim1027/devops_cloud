f6e54714ff87e7550886cb5c56734d70b29d8647
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 10:54:28 +0900

    blog 앱 생성, settings에 추가

--- mydjango23_2/mydjango23_2/settings.py
+++ mydjango23_2/mydjango23_2/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third apps
+    'bootstrap5',
+
+    # local apps
+    'blog',
 ]
 
 if DEBUG:
