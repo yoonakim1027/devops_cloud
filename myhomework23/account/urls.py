8c9feeaf9c1c6ba45edbb68a313529499b6ddcba
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 23:33:40 +0900

    폼 개선

--- myhomework23/account/urls.py
+++ myhomework23/account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from account import views
 from blog.urls import urlpatterns
 
 app_name = "account"
 
 urlpatterns =[
-    path()
+
+    path('signup/',views.signup_form, name="signup_form"),
 ]
--- myhomework23/account/views.py
+++ myhomework23/account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.db import models
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def signup_form(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("account:signup_form")
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,"accounts/signup_form.html",{
+            "form":form,
+        }
+    )
