5f1f5c7ab4a33febeabef1acce88c357d66bb1a4
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-15 10:21:49 +0900

    MEDIA 설정 (settings.py, urls.py)

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -1,9 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+
 
 if settings.DEBUG:
     import debug_toolbar
