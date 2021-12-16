f9817426723821ede74632402050a25aeee5fda6
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-16 18:28:37 +0900

    MEDIA 설정 (settings.py, urls.py)

--- myhomework24/myhomework24/urls.py
+++ myhomework24/myhomework24/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,6 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
 
 
