3ab48f72ac7173532352f3c44c94bb8f5088dad2
Kim yoona <20162921@gm.hannam.ac.kr>
2021-12-20 20:26:55 +0900

    java scripts (for문을 사용한 타자연습)

---
+++ myproj26/myhomework03.js
@@ -0,0 +1,48 @@
+// 필요한 것 : 우선 랜덤값 인출을 위한 리스트
+const readlineSync = require("readline-sync");
+const animals = ["cat", "dog", "fox", "monkey", "mouse", "panda", "frog", "snake", "wolf"];
+
+// 현재 시간 출력
+// let -> 선언한 다음에 바뀔 수 있는 변수
+// const -> 상수
+
+let startTime = new Date();
+console.log("현재 시작 시간 : " + startTime);
+
+
+function shuffle(array){
+    array.sort(()=> Math.random()- 0.5);
+}
+shuffle(animals);
+
+let counter = 0;
+// 파이썬의 f스트링 같은 용법 : `${변수명}`
+for (let i = 0; i <5; i++){
+    let shuffleAnimal = animals[i];
+    console.log("♥ 따라 입력해보세요 ♥");
+    const {question} = require("readline-sync");
+    const input = question(`${shuffleAnimal} : ` );
+
+    if (shuffleAnimal === input) {
+        counter++;
+        console.log("정확합니다.");
+    }
+    else {
+        console.log("오타가 있어요.");
+    }
+
+}
+
+//STtime
+let finishTime = new Date();
+let FTtime = finishTime.getTime();
+let STtime = startTime.getTime();
+
+// Math.floor -> 소숫점 자리 삭제. (올림, 내림 X 그냥 삭제)
+let number = Math.floor((FTtime - STtime) / 1000);
+
+console.log(`총 ${counter}번 성공하셨습니다.`);
+
+console.log(`총 ${number}초가 걸리셨어요.`);
+
+
