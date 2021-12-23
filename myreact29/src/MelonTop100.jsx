c2958dd45c9a2d8bd1adbdf6999be014d8a1d8cd
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2021-12-23 14:12:26 +0900

    css 간단적용

--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -11,53 +11,25 @@
 */
 
 import { useState } from 'react';
-
+import initialSongList from 'data/melon_data.json';
+import 'MelonTop100.css';
 // 전역변수 : 바뀌지 않는 데이터
-const initialSongList = [
-  {
-    'Unnamed: 0': 0,
-    song_no: 32872978,
-    title: 'Dynamite',
-    album: 'Dynamite (DayTime Version)',
-    artist: '방탄소년단',
-    rank: 1,
-    like: 354655,
-  },
-  {
-    'Unnamed: 0': 1,
-    song_no: 33077590,
-    title: 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)',
-    album: '쇼미더머니 9 Episode 1',
-    artist: '미란이',
-    rank: 2,
-    like: 40716,
-  },
-  {
-    'Unnamed: 0': 2,
-    song_no: 32998018,
-    title: '힘든 건 사랑이 아니다',
-    album: '힘든 건 사랑이 아니다',
-    artist: '임창정',
-    rank: 3,
-    like: 74236,
-  },
-];
 
 // ul -> 순서가 없는 리스트를 만들때 사용
 // ul>li*3 하고 엔터를 하면 li가 세줄이 자동생성
 // const handleClick = () =>{};  -> arrow function
 function MelonTop100() {
-  const [SongList, setSongList] = useState([]); // songlist를 담을 것. -> 빈 [array]에서 시작
+  const [songList, setSongList] = useState([]); // songlist를 담을 것. -> 빈 [array]에서 시작
   const handleClick = () => {
-    setSongList(initialSongList);
+    setSongList(initialSongList); // 현재 상수
   };
 
   return (
     <div>
       <h2>멜론 top 100</h2>
       <button onClick={handleClick}>로딩</button>
-      <ul>
-        {SongList.map((song) => {
+      <ul className="songList">
+        {songList.map((song) => {
           // 한곡씩(song)나옴
           return <li>{song.title}</li>;
         })}
