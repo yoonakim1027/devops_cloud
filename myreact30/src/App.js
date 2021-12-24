96d2aae5a0b810d3f8dc8c75297bea61266d5169
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2021-12-24 13:28:27 +0900

    카운터 스타일 적용

--- myreact30/src/App.js
+++ myreact30/src/App.js
@@ -1,8 +1,25 @@
 import Counter from 'Counter';
+//import { Fragment } from 'react'; // 예전엔 이렇게 썼는데~
+// 이제는 그냥 안쓰고 <> </> 이렇게 태그 안에를 비워도 된다 ~
+
 // 여기서 부모는 Counter.jsx의 counter
 
 function App() {
-  return <Counter initial={10} />; // 속성으로 initial = 10
+  return (
+    <>
+      <Counter initial={10} color={'blue'} />
+      <Counter initial={10} color={'red'} />
+      <Counter initial={10} color={'green'} />
+    </>
+  );
+  // div로 안감싸고 여러개를 하면 오류남!
+  // 한 묶음으로 해줘야 ㅎ오류가 안난다 .
+  //리액트의 제약 사항 : 한개를 반환해야 한다 .
+
+  // 그냥 태그명안쓰고 <> </> 라고 하면? div 없이도 가능
+  // Fragment -> 실제로 그려지는 것은 아니고, 논리적으로 한 묶음이라는 것을 표현하는 것
+
+  // 속성으로 initial = 10
 }
 // 부모가 자식에게 key value를 넘김 -> 이를 속성값 이라고 함
 // 인자에대한 이름이 initial => 인자에 대한 값이 {10}
--- myreact30/src/components/Counter.jsx
+++ myreact30/src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+// 임의의 속성값 color 추가 -> 문자열
 
-function Counter({ initial }) {
+function Counter({ initial, color }) {
   //속성값을 초기값으로 참조하여 상탯값 value를 생성
   const [value, setValue] = useState(initial); // 초기값(상탯값) 지정
 
@@ -15,15 +16,19 @@ function Counter({ initial }) {
     setValue(value - 1);
   };
 
+  // style에 추가로 backgroundColor : color를 추가하려면 ?
+  // ...style,
+  //  backgroundColor
   return (
     <div
-      style={{ userSelect: 'none' }}
+      style={{ ...style, backgroundColor: color }}
       onClick={handleClick}
       onContextMenu={handleContextMenu}
     >
       {value}
     </div>
   );
+  // 함수의 인자는 대입된적이 없으면 undefiend
   // <상탯값 useState 설정 후 >
   // 여기서는 값을 initial로 참조할 게아니라, 위의 value 값을 받으면 됨!
   // 여기의 value는 위의 const에서 정의한 value
@@ -35,6 +40,27 @@ function Counter({ initial }) {
   // 이 인자에 기반해서 내가 이함수를 화면에 어떻게 보여주겠다는 것을 만들어서 리턴해주면
   // 이 리턴값이 화면에 보여지게 됨
 }
+
+// 컴포넌트의 고정된 스타일을 지정할 때 사용
+const style = {
+  width: '100px',
+  height: '100px',
+  borderRadius: '50px',
+  lineHeight: '100px',
+  textAlign: 'center',
+  display: 'inline-block',
+  fontSize: '3rem',
+  margin: '1rem',
+  userSelect: 'none',
+};
+
 export default Counter;
 
 // ContextMenu
+
+/* 
+< 리액트에서 스타일 설정하는 방법 >
+
+1. 외부에 스타일에 대한 오브젝트 선언
+const style ~ 이렇게 선언 -> 변경은 안됨 
+*/
