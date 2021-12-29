458846ddea5ff9a8d67d65a04ee9e226d2e19470
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2021-12-29 16:10:47 +0900

    profile-list.json 조회에 실패 시, 에러메세지 출력  - error 새 상탯값 정의  - 매 통신 시 에러 초기화

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -6,7 +6,15 @@ function PageProfile() {
   const [profileList, setProfileList] = useState([]); // useState는 초깃값으로 []빈 어레이를 초깃값으로 지정
 
   //Arrow function을 사용하기 위해선? const 변수명 () => {내용}
+
+  // Stage 7. profile-list.json 조회에 실패했을 때,
+  // "조회 시에 오류가 발생했습니다. 잠시 후 다시 시도해주세요." 라는 에러메세지를 띄워줍니다.
+  // - 에러 여부를 기록하는 error 상탯값을 추가로 정의하고, 디폴트값은 null로 둡니다.
+  //- 에러가 발생하면 에러객체를 저장하고, 통신이 시작될 때에는 에러 상탯값을 null로 초기화합니다.
+  // - 본 동작을 테스트하기 위해, 조회 URL을 엉뚱하게 변경하고, 새로고침 버튼을 눌러 에러메세지 노출을 테스트합니다.
+  const [error, setError] = useState(null);
   const handleRefresh = () => {
+    setError(null); // 매 통신시 에러 초기화
     Axios.get(
       'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
     )
@@ -23,13 +31,14 @@ function PageProfile() {
         // response.data<-  위에서 .get으로 받아온 데이터가 들어있음!
       })
       .catch((error) => {
-        console.error(error);
+        setError(error);
       });
     // 애초에 Axios 과정 자체를 리턴하게 되는 것
   };
 
   // 처음 실행 시, 새로 고침 시 바로 행해지는 함수
   // yarn start 해서 띄울때 바로 실행하기 위한 함수
+  // 마운트는 한번...
   useEffect(() => {
     handleRefresh();
   }, []);
@@ -51,8 +60,10 @@ function PageProfile() {
       <button onClick={handleRefresh}>Refresh</button>
       {/* 선택적 랜더링. && 앞에 있는 조건이 맞으면 뒤에 것을 렌더링(화면에 보여지는것) */}
       {/* .length -> 배열의 길이를 반환(배열은 순서가 있음) */}
-      {profileList.length === 0 && <h4>"등록된 프로필이 없습니다." </h4>}
-
+      {profileList.length === 0 && <h4>등록된 프로필이 없습니다.</h4>}
+      {error !== null && (
+        <h3>조회 시에 오류가 발생했습니다. 잠시 후 다시 시도해주세요.</h3>
+      )}
       {profileList.map((profile) => {
         return (
           <div key={profile.uniqueId}>
