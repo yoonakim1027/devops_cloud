1c6d01e446320c97228341d6b8a27a2dc8d79966
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-05 10:41:06 +0900

    index.js 불필요한 코드 제거, Counter 포인터 간단 구현(useState)

--- myreact37/src/components/Counter.jsx
+++ myreact37/src/components/Counter.jsx
@@ -1,14 +1,20 @@
-import { useStae } from 'react';
+import { useState } from 'react';
 import './Counter.css';
 
 function Counter() {
-  const [value, setValue] = useStae(0);
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+
   return (
     <div
       className="counter"
       style={{
         backgroundColor: 'red',
       }}
+      onClick={handleClick}
     >
       {value}
     </div>
