79d24e30319f7d8d24fa1473e01e31f176d62ba4
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-05 18:45:26 +0900

    기본 todo 구성, css기본 색상 참조

--- mystudy37_2/src/components/Todo.jsx
+++ mystudy37_2/src/components/Todo.jsx
@@ -1,5 +1,15 @@
 import './Todo.css';
 
-function Todo() {}
+function Todo({ todo, onClick }) {
+  return (
+    <div
+      className={`m-1 p-1 rounded-lg text-lg border-blue-200 border-2 hover:border-blue-500 hover:scale-105 cursor-pointer text-white`}
+      style={{ backgroundColor: todo.color }}
+      onClick={onClick}
+    >
+      {todo.content}
+    </div>
+  );
+}
 
 export default Todo;
