a2e2883ce20aa84c75af4b9c475c3843ac5e2406
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-05 15:00:58 +0900

    setter 함수 지정방식

--- myreact37/src/components/TodoList.jsx
+++ myreact37/src/components/TodoList.jsx
@@ -22,43 +22,18 @@ function TodoList() {
     color: 'Orange',
   });
 
-  // const changedInputText = (e) => {
-  //   setInputText(e.target.value);
-  //   // 값을 얻어오는 것
-  //   //e는 이벤트 객체
-  // };
-
-  // const appendInputText = (e) => {
-  //   console.log('e.key:', e.key);
-  //   if (e.key === 'Enter') {
-  //     console.log('inputText:', inputText);
-  //     setTodoList((prevTodoList) => {
-  //       return [...prevTodoList, { content: inputText }];
-  //     }); // ... -> 원래 있던 배열 불러오기
-  //     // 객체 {}
-  //     // 위에서 {content : ''} 이런식으로 객체를 넣어줬기 때문에
-  //     // 여기서도 {content: inputText} 이렇게 넣은 것
-
-  //     setInputText('');
-
-  //     //1. todoList 배열 끝에 inputText를 추가합니다
-  //     // 2. inputText를 다시 비웁니다
-  //     // => 현재는 input 박스 UI가 비어보이진 않을 거에요!
-  //     // 수정이 아님! setTodoList에 함수를 넘기는 것! (이게 좋은 방식
-  //     // todoList의 상탯값을 변경하는 것 X 배열의 push사용 X
-  //   }
-  // };
-
   // 현재 fieldValues에 todo내역이 저장되어 있음
   const appendTodo = () => {
-    // setter에 값 지정방식
     console.log('새로운 TODO를 추가하겠습니다');
     const todo = { ...fieldValues }; // 오브젝트
-    // 배열 !
-    setTodoList([
-      ...todoList, //기존의 todoList 추가
-      todo,
-    ]);
+    //  setter에 값 지정방식 : 배열
+    // setTodoList([
+    //   ...todoList, //기존의 todoList 추가
+    //   todo,
+    // ]);
+
+    // setter에 함수 지정 방식
+    setTodoList((prevTodoList) => [...prevTodoList, todo]);
     clearFieldValues();
   };
 
@@ -87,12 +62,6 @@ function TodoList() {
         clear
       </button>
 
-      {/* <input
-        type="text"
-        value={inputText}
-        onChange={changedInputText}
-        onKeyPress={appendInputText}
-      /> */}
       {todoList.map((todo, index) => (
         <Todo todo={todo} onClick={() => removeTodo(index)} />
         // <div onClick={() => removeTodo(index)}>{todo.content}</div>
