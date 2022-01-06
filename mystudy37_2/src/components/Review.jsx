41b7297dda434fae3004d249776807c3798f4fec
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-06 13:23:22 +0900

    커서가 올라가면 수정, 삭제가 보여지도록 구현

--- mystudy37_2/src/components/Review.jsx
+++ mystudy37_2/src/components/Review.jsx
@@ -1,13 +1,23 @@
 import './Review.css';
 import Rating from './Rating';
-
+import { useState } from 'react';
 function Review({ review, onClick }) {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
       className={`m-1 p-1 rounded-lg text-lg border-green-500 border-2 hover:scale-105 cursor-pointer `}
       style={{ backgroundColor: 'white' }}
       onClick={onClick}
     >
+      {showMenu && (
+        <div>
+          <span>수정</span>
+          <span>삭제</span>
+        </div>
+      )}
+
       {review.content}
 
       <Rating score={review.rating} />
