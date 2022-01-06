ba9e294b9fea57de0326d24445ed47958c6599f5
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-06 13:32:25 +0900

    커서를 수정, 삭제에 올렸을 때(hover) 변환 구현

--- mystudy37_2/src/components/Review.jsx
+++ mystudy37_2/src/components/Review.jsx
@@ -2,19 +2,23 @@ import './Review.css';
 import Rating from './Rating';
 import { useState } from 'react';
 function Review({ review, onClick }) {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   return (
     <div
       onMouseEnter={() => setShowMenu(true)}
       onMouseLeave={() => setShowMenu(false)}
-      className={`m-1 p-1 rounded-lg text-lg border-green-500 border-2 hover:scale-105 cursor-pointer `}
+      className={` m-1 p-1 pt-5 rounded-lg text-lg border-green-500 border-2 hover:scale-105 relative`}
       style={{ backgroundColor: 'white' }}
       onClick={onClick}
     >
       {showMenu && (
-        <div>
-          <span>수정</span>
-          <span>삭제</span>
+        <div className="text-xs absolute top-0 right-0">
+          <span className="text-green-700 hover:bg-green-300 cursor-pointer mr-1">
+            수정
+          </span>
+          <span className="text-red-700  hover:bg-red-300 cursor-pointer mr-1">
+            삭제
+          </span>
         </div>
       )}
 
